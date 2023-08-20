8eed9761ad48bc8bc7ac9ecf40389f0f68f58faa
Hong Quan <hongquan.contact@gmail.com>
2023-08-20 17:01:57 +0700

    update

--- src/pages/Customer/VerifyPayment/verifyPayment.jsx
+++ src/pages/Customer/VerifyPayment/verifyPayment.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from "../../../components/Header/header"
 import LoadingOverlay from "../../../components/Loading/LoadingOverlay"
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { verifyOrder } from '../../../services/paymentService';
 
 const VerifyPayment = () => {
@@ -10,6 +10,7 @@ const VerifyPayment = () => {
     const [OrderID, setOrderID] = useState("");
     const [loading, setLoading] = useState(true);
     const [status, setStatus] = useState(false);
+    const navigate = useNavigate();
     useEffect(() => {
         setOrderID(id);
         setTimeout(() => {
@@ -17,45 +18,69 @@ const VerifyPayment = () => {
             verify();
         }, 15000);
     }, []);
+
     const verify = async () => {
         let res = await verifyOrder(id);
         if (res) {
             setStatus(res);
         }
+        setTimeout(() => {
+            navigate("/transaction");
+        }, 5000);
     }
+
     return (
         <>
-            <Header></Header>
-            <LoadingOverlay loading={loading}></LoadingOverlay>
-            {loading == false &&
+            <Header />
+            <LoadingOverlay loading={loading} />
 
-                <div className='d-flex' style={{ flexDirection: 'column', alignItems: 'center', textAlign: 'center', height: '90vh', backgroundColor: '#fff' }}>
-                    <div style={{ margin: 'auto 0' }}>
-                        <span style={{ fontSize: '16px', fontWeight: '600' }}>Order ID: {OrderID}</span>
+            {!loading && (
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    height: '90vh',
+                    backgroundColor: '#fff'
+                }}>
+                    <span style={{ fontSize: '16px', fontWeight: '600' }}>Order ID: {OrderID}</span>
 
-                        {status == true ? (
+                    {status ? (
+                        <div style={{
+                            marginTop: '20px',
+                            backgroundColor: 'green',
+                            color: 'white',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: '50%',
+                            width: '100px',
+                            height: '100px'
+                        }}>
+                            <i className="fa-solid fa-check" style={{ fontSize: '36px' }}></i>
+                        </div>
+                    ) : (
+                        <div style={{
+                            marginTop: '20px',
+                            color: 'white',
+                            backgroundColor: 'red',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: '50%',
+                            width: '100px',
+                            height: '100px'
+                        }}>
+                            <i className="fa-solid fa-xmark" style={{ fontSize: '36px' }}></i>
+                        </div>
+                    )}
 
-
-                            <div>
-                                <div className='m-3' style={{ backgroundColor: 'green', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', width: '100px', height: '100px' }}>
-                                    <i className="fa-solid fa-check" style={{ fontSize: '36px' }}></i>
-                                </div>
-                                <span>Payment success !</span>
-                            </div>
-                        ) : (
-                            <div >
-                                <div className='mt-3' style={{ color: 'white', backgroundColor: 'red', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', width: '100px', height: '100px' }}>
-                                    <i className="fa-solid fa-xmark" style={{ fontSize: '36px' }}></i>
-                                </div>
-                                <span className='mt-3 d-block'><b>Payment failed !</b></span>
-                            </div>
-                        )}
-
-
-                    </div>
+                    <span className="mt-3">
+                        <b>{status ? "Payment success !" : "Payment failed !"}</b>
+                    </span>
                 </div>
-            }
-
+            )}
         </>
     );
 };
