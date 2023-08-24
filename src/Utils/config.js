3c8b5a6af10ea1b151404517b926a09a947325f1
Hong Quan <hongquan.contact@gmail.com>
2023-08-24 16:58:54 +0700

    update

--- src/Utils/config.js
+++ src/Utils/config.js
@@ -1,4 +1,4 @@
-// const hosting = "https://monshop.azurewebsites.net";
-const hosting = "https://localhost:7022";
+const hosting = "https://monshop.azurewebsites.net";
+// const hosting = "https://localhost:7022";
 
 export default hosting;
